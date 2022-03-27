a0acadd9e3fb8f94b23ed01fe8504c341d82333a
hugosenari <hugosenari@gmail.com>
2022-03-27 17:58:20 -0300

    chore: re index awslambda

--- master/flake.nix
+++ master/flake.nix
@@ -11,6 +11,7 @@
   inputs.src-awslambda-master.ref   = "refs/heads/master";
   inputs.src-awslambda-master.owner = "lambci";
   inputs.src-awslambda-master.repo  = "awslambda.nim";
+  inputs.src-awslambda-master.dir   = "";
   inputs.src-awslambda-master.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
